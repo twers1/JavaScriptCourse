c1bf2a2689e9b1462ebe5f39a36e0732c3a103e1
Sima Tarverdyan <102418053+twers1@users.noreply.github.com>
2022-07-04 12:36:18 +0300

    Create 5.строки4.js

---
+++ 2. Стандартные объекты/5.строки4.js
@@ -0,0 +1,22 @@
+// Условие: В этом задании в нашу функцию testStr передаются две строки. Вам нужно вернуть индекс позиции, с которой начинается вхождение второй строки в первую.
+
+// Sample Input 1:
+
+// This is a test string
+// test
+// Sample Output 1:
+
+// 10
+// Sample Input 2:
+
+// This is a test string for testing
+// test
+// Sample Output 2:
+
+// 10
+
+
+// Решение: 
+function testStr(a, b) {
+    return a.indexOf(b);
+}
